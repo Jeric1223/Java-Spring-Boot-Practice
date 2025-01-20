73b26fd98b1f5f4d9a4de60052f1d3d37a000f19
Jeric1223 <soehd0888@naver.com>
2025-01-20 21:18:54 +0900

    section 4 : 회원 서비스 개발

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     * @return
+     */
+    public Long join(Member member){
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름을 가진 중복 회원은 가입이 안되도록
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                 throw new IllegalStateException("이미 존재하는 회원 입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers () {
+       return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
